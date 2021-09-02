076ef59d2a289486421a204cf65403b6df59ad93
Nadir Bakkali <nadir.bakkali@outlook.com>
2021-09-02 08:42:27 +0200

    update of main class

--- src/com/bknadir/Main.java
+++ src/com/bknadir/Main.java
@@ -6,5 +6,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 	// write your code here
+        Deck deck = new Deck(true);
+        System.out.println(deck);
+        deck.randomize();
+        System.out.println(deck);
+        deck.sort();
+        System.out.println(deck);
     }
 }
